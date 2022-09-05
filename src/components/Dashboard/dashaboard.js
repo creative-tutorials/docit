c22141a0754df7c9a0d4414783e099218f766516
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-09-05 21:31:49 +0100

    🚧 Work in Progress

--- src/components/Dashboard/dashaboard.js
+++ src/components/Dashboard/dashaboard.js
@@ -1,7 +1,46 @@
+import { calculateFileSize } from "../Calculate/Calculate";
+import { elemeState } from "../elements/elem";
+import { darkTheme } from "../theme/dark/darkThme";
+import { lightTheme } from "../theme/light/lightTheme";
 import "../../styles/Dash.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIRBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MESUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics);
+// Create a root reference
+const storage = getStorage();
+const db = getFirestore(app);
 function DashboardComponent() {
-  const [theme, settheme] = useState();
+  const [theme, settheme] = useState("light");
+  useEffect(() => {
+    LoadPage();
+
+    // fetchUploadContent();
+    return () => {
+      if (!fetchUploadContent) {
+        console.log("x");
+      } else {
+        return fetchUploadContent();
+      }
+    };
+  });
+
   const folders_box = useRef();
   const folders_box2 = useRef();
   const folders_box3 = useRef();
@@ -29,132 +68,375 @@ function DashboardComponent() {
   const logout = useRef();
   const theme_change = useRef();
 
-  const upload_box = useRef();
-  const upload_box2 = useRef();
-  const upload_box3 = useRef();
-  const upload_box4 = useRef();
+  const uploadBtn = useRef();
 
+  const fileInput = useRef();
+
+  const pop_content = useRef();
+
+  const txtContent = useRef();
+
+  const filename = useRef();
+  const filesize = useRef();
+
+  const io_upload = useRef();
   const colorsPallet = {
     lightBG: "#f1f7fc",
     darkBG: "#1B2430",
-    dark: "#1B262C",
-    blueBG: "#458ce8",
-    greyBG: "#3a4447",
-    white: "#FFFFFF",
+
+    /**
+     * The changeTheme function changes the theme of the app.
+     *
+     *
+     *
+     * @return The theme
+     *
+     */
   };
   const changeTheme = () => {
-    const folderA = folders_box.current;
-    const folderB = folders_box2.current;
-    const folderC = folders_box3.current;
-
-    const nameFolderA = nameFolder.current;
-    const nameFolderB = nameFolder2.current;
-    const nameFolderC = nameFolder3.current;
-
-    const size_textA = sizeRef.current;
-    const size_textB = sizeRef2.current;
-    const size_textC = sizeRef3.current;
-
-    const nutTxt = nutText.current;
-    const header_title = jump_header_title.current;
-
-    const queryStyle = inputQuery.current;
-
-    const newSidebar = sidebar.current;
-
-    const rgt_text = rightText.current;
-    const rgt_text2 = rightText2.current;
-    const rgt_text3 = rightText3.current;
-    const rgt_text4 = rightText4.current;
-
-    const logout_btn = logout.current;
-    const theme_btn = theme_change.current;
-
-    const upldBx = upload_box.current;
-    const upldBx2 = upload_box2.current;
-    const upldBx3 = upload_box3.current;
-    const upldBx4 = upload_box4.current;
+    const {
+      folderA,
+      folderB,
+      folderC,
+      nameFolderA,
+      nameFolderB,
+      nameFolderC,
+      size_textA,
+      size_textB,
+      size_textC,
+      nutTxt,
+      header_title,
+      queryStyle,
+      newSidebar,
+      rgt_text,
+      rgt_text2,
+      rgt_text3,
+      rgt_text4,
+      logout_btn,
+      theme_btn,
+      upldBtn,
+    } = elemeState(
+      folders_box,
+      folders_box2,
+      folders_box3,
+      nameFolder,
+      nameFolder2,
+      nameFolder3,
+      sizeRef,
+      sizeRef2,
+      sizeRef3,
+      nutText,
+      jump_header_title,
+      inputQuery,
+      sidebar,
+      rightText,
+      rightText2,
+      rightText3,
+      rightText4,
+      logout,
+      theme_change,
+      uploadBtn
+    );
     if (theme === "light") {
       settheme("dark");
       document.querySelector("body").style.backgroundColor =
         colorsPallet.darkBG;
-      folderA.classList.add("setDark");
-      folderB.classList.add("setDark");
-      folderC.classList.add("setDark");
-
-      nameFolderA.classList.add("setDark");
-      nameFolderB.classList.add("setDark");
-      nameFolderC.classList.add("setDark");
-
-      size_textA.classList.add("setDark");
-      size_textB.classList.add("setDark");
-      size_textC.classList.add("setDark");
-
-      nutTxt.classList.add("setDark");
-      header_title.classList.add("setDark");
-
-      queryStyle.classList.add("setDark");
-      newSidebar.classList.add("setDark");
-
-      rgt_text.classList.add("setDark");
-      rgt_text2.classList.add("setDark");
-      rgt_text3.classList.add("setDark");
-      rgt_text4.classList.add("setDark");
-
-      logout_btn.classList.add("setDark");
-      theme_btn.classList.add("setDark");
-
-      upldBx.classList.add("setDark");
-      upldBx2.classList.add("setDark");
-      upldBx3.classList.add("setDark");
-      upldBx4.classList.add("setDark");
+      darkTheme(
+        folderA,
+        folderB,
+        folderC,
+        nameFolderA,
+        nameFolderB,
+        nameFolderC,
+        size_textA,
+        size_textB,
+        size_textC,
+        nutTxt,
+        header_title,
+        queryStyle,
+        newSidebar,
+        rgt_text,
+        rgt_text2,
+        rgt_text3,
+        rgt_text4,
+        logout_btn,
+        theme_btn,
+        upldBtn
+      );
     } else {
       settheme("light");
       document.querySelector("body").style.backgroundColor =
         colorsPallet.lightBG;
-      folderA.classList.remove("setDark");
-      folderB.classList.remove("setDark");
-      folderC.classList.remove("setDark");
-
-      nameFolderA.classList.remove("setDark");
-      nameFolderB.classList.remove("setDark");
-      nameFolderC.classList.remove("setDark");
-
-      size_textA.classList.remove("setDark");
-      size_textB.classList.remove("setDark");
-      size_textC.classList.remove("setDark");
-
-      nutTxt.classList.remove("setDark");
-      header_title.classList.remove("setDark");
-
-      queryStyle.classList.remove("setDark");
-      newSidebar.classList.remove("setDark");
-
-      rgt_text.classList.remove("setDark");
-      rgt_text2.classList.remove("setDark");
-      rgt_text3.classList.remove("setDark");
-      rgt_text4.classList.remove("setDark");
-
-      logout_btn.classList.remove("setDark");
-      theme_btn.classList.remove("setDark");
-
-      upldBx.classList.remove("setDark");
-      upldBx2.classList.remove("setDark");
-      upldBx3.classList.remove("setDark");
-      upldBx4.classList.remove("setDark");
+      lightTheme(
+        folderA,
+        folderB,
+        folderC,
+        nameFolderA,
+        nameFolderB,
+        nameFolderC,
+        size_textA,
+        size_textB,
+        size_textC,
+        nutTxt,
+        header_title,
+        queryStyle,
+        newSidebar,
+        rgt_text,
+        rgt_text2,
+        rgt_text3,
+        rgt_text4,
+        logout_btn,
+        theme_btn,
+        upldBtn
+      );
     }
     if (theme === undefined) {
-      settheme("light");
+      settheme("dark");
     }
-    console.log(theme);
+    // console.log(theme);
     localStorage.setItem("theme", theme);
     const themeStorage = localStorage.getItem("theme");
     if (themeStorage === "light") {
-      console.log("storage set theme to", theme);
+      console.log("previous theme was", theme);
+      settheme("dark");
+      localStorage.setItem("theme", "dark");
     } else if (themeStorage === "dark") {
-      console.log("storage set theme to", theme);
+      console.log("previous theme was", theme);
+      settheme("light");
+      localStorage.setItem("theme", "light");
     }
   };
+  const LoadPage = () => {
+    console.log("load");
+    const theme = localStorage.getItem("theme");
+    const {
+      folderA,
+      folderB,
+      folderC,
+      nameFolderA,
+      nameFolderB,
+      nameFolderC,
+      size_textA,
+      size_textB,
+      size_textC,
+      nutTxt,
+      header_title,
+      queryStyle,
+      newSidebar,
+      rgt_text,
+      rgt_text2,
+      rgt_text3,
+      rgt_text4,
+      logout_btn,
+      theme_btn,
+      upldBtn,
+    } = elemeState(
+      folders_box,
+      folders_box2,
+      folders_box3,
+      nameFolder,
+      nameFolder2,
+      nameFolder3,
+      sizeRef,
+      sizeRef2,
+      sizeRef3,
+      nutText,
+      jump_header_title,
+      inputQuery,
+      sidebar,
+      rightText,
+      rightText2,
+      rightText3,
+      rightText4,
+      logout,
+      theme_change,
+      uploadBtn
+    );
+    if (theme === "dark") {
+      console.log("theme is dark");
+      document.querySelector("body").style.backgroundColor =
+        colorsPallet.darkBG;
+      darkTheme(
+        folderA,
+        folderB,
+        folderC,
+        nameFolderA,
+        nameFolderB,
+        nameFolderC,
+        size_textA,
+        size_textB,
+        size_textC,
+        nutTxt,
+        header_title,
+        queryStyle,
+        newSidebar,
+        rgt_text,
+        rgt_text2,
+        rgt_text3,
+        rgt_text4,
+        logout_btn,
+        theme_btn,
+        upldBtn
+      );
+    } else {
+      document.querySelector("body").style.backgroundColor =
+        colorsPallet.lightBG;
+      lightTheme(
+        folderA,
+        folderB,
+        folderC,
+        nameFolderA,
+        nameFolderB,
+        nameFolderC,
+        size_textA,
+        size_textB,
+        size_textC,
+        nutTxt,
+        header_title,
+        queryStyle,
+        newSidebar,
+        rgt_text,
+        rgt_text2,
+        rgt_text3,
+        rgt_text4,
+        logout_btn,
+        theme_btn,
+        upldBtn
+      );
+    }
+  };
+  const handleFileUpload = (e) => {
+    /**
+   * The handleFileUpload function is used to upload a file from the user's computer
+   * to Firebase Storage. It takes in an event object as a parameter, which contains
+   * information about the file that was uploaded. The function then uses FileReader
+   * and uploadBytes functions to read the contents of the file and upload it to Firebase. 
+   
+   *
+   * @param e Get the file that is being uploaded
+   *
+   * @return The value of the text content
+   *
+   * @docauthor Trelent
+   */
+    /** @type {*} */
+    const text = txtContent.current;
+    const fileName = filename.current;
+    const fileSize = filesize.current;
+    const upldBtn = uploadBtn.current;
+    const popup = pop_content.current;
+    const io_uploads = io_upload.current;
+    const targetName = e.target.files[0].name;
+    const targetSize = e.target.files[0].size;
+    const reader = new FileReader();
+    console.log(reader);
+    // use File Reader
+    const files = fileInput.current.files.item(0);
+    console.log(files);
+    // Create a reference to 'mountains.jpg'
+    const storageRef = ref(storage, "uploads/" + targetName);
+    text.textContent = "Uploading";
+    upldBtn.classList.add("active");
+
+    uploadBytes(storageRef, files).then((snapshot) => {
+      console.log("Uploaded a blob or file!");
+      text.textContent = "Upload";
+      upldBtn.classList.remove("active");
+
+      // function humanFileSize(bytes, si = false, dp = 1) {
+
+      // }
+      const humanFileSize = calculateFileSize();
+
+      changeElementSate();
+      sendFileRequest();
+
+      const docRef = addDoc(collection(db, "uploads"), {
+        filename: targetName,
+        size: humanFileSize(targetSize),
+      });
+      console.log("Document written with ID: ", docRef.id);
+      async function sendFileRequest() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+          filename: targetName,
+          filesize: humanFileSize(targetSize),
+        });
+
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow",
+        };
+
+        fetch("http://localhost:4000/uploads/upld", requestOptions)
+          .then((response) => response.json())
+          .then((result) => console.log(result))
+          .catch((error) => console.log("error", error));
+      }
+      async function changeElementSate() {
+        const createNew = document.createElement("div");
+        createNew.innerHTML = `<div class="upload_box upload_boxs">
+        <i class="fa-solid fa-file-zipper"></i>
+        <h4>${targetName}</h4>
+      </div>`;
+        io_uploads.appendChild(createNew);
+        fileName.innerHTML = targetName;
+        fileSize.innerHTML = humanFileSize(targetSize);
+        popup.classList.add("active");
+        setTimeout(() => {
+          popup.classList.remove("active");
+        }, 3000);
+      }
+    });
+
+    reader.onload = (e) => {
+      console.log(reader.result);
+    };
+    reader.readAsDataURL(files);
+  };
+  const fetchUploadContent = () => {
+    const io_uploads = io_upload.current;
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:4000/uploads", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.table(result);
+        const newResult = result;
+        newResult.forEach((item) => {
+          console.log("item", item);
+          const createNew = document.createElement("div");
+          createNew.innerHTML = `<div class="upload_box upload_boxs">
+          <i class="fa-solid fa-file-zipper"></i>
+          <h4>${item.filename}</h4>
+        </div>`;
+          io_uploads.appendChild(createNew);
+        });
+      })
+      .catch((error) => console.log("error", error));
+  };
+  /**
+   * The CheckAuth function checks to see if the user is logged in. If they are not, it redirects them to the signup page.
+   *
+   *
+   *
+   * @return A boolean value
+   */
+  const CheckAuth = () => {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+      window.location.pathname = "/signup";
+    }
+  };
+  
+  CheckAuth();
   return (
     <div className="dashboard">
       <div className="container">
@@ -274,34 +556,38 @@ function DashboardComponent() {
                   All Files
                 </h2>
               </div>
-              <div className="io_upload">
-                <div className="upload_box" ref={upload_box}>
-                  <i className="fa-solid fa-file-pdf"></i>
-                  <h4>Test.pdf</h4>
-                </div>
-                <div className="upload_box" ref={upload_box2}>
-                  <i className="fa-solid fa-file-video"></i>
-                  <h4>Movie.mp4</h4>
-                </div>
-                <div className="upload_box" ref={upload_box3}>
-                  <i className="fa-solid fa-file-word"></i>
-                  <h4>secrets.docx</h4>
-                </div>
-                <div className="upload_box" ref={upload_box4}>
-                  <i className="fa-solid fa-file-zipper"></i>
-                  <h4>app.zip</h4>
-                </div>
-              </div>
+              <div className="io_upload" ref={io_upload}></div>
             </div>
           </div>
         </div>
       </div>
-      <div className="upload_btn">
-        <input type="file" name="" id="upload" hidden />
+      <div className="upload_btn" ref={uploadBtn}>
+        <input
+          type="file"
+          name=""
+          id="upload"
+          hidden
+          onChange={handleFileUpload}
+          ref={fileInput}
+        />
         <label htmlFor="upload">
           <i className="fa-solid fa-cloud-arrow-up"></i>
-          <h4>Upload</h4>
+          <h4 ref={txtContent}>Upload</h4>
         </label>
+      </div>
+      <div className="pop_up" ref={pop_content}>
+        <div className="pop_content">
+          <div className="filename">
+            {" "}
+            <h5>Uploaded</h5>{" "}
+            <span id="filename" ref={filename}>
+              Test.txt
+            </span>
+          </div>
+          <div className="filesize" ref={filesize}>
+            2.4kb
+          </div>
+        </div>
       </div>
     </div>
   );
